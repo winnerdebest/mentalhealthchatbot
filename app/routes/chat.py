246864a488016a15c1f9db33c3b1bf511cba8e23
Winner <winnerbrown9@gmail.com>
2025-07-12 11:08:26 +0100

    Can't say it didnt surprise me

--- app/routes/chat.py
+++ app/routes/chat.py
@@ -16,10 +16,10 @@ router = APIRouter()
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 genai.configure(api_key=GEMINI_API_KEY)
 
-print("Available Models:")
+"""print("Available Models:")
 for m in genai.list_models():
     if "generateContent" in m.supported_generation_methods:
-        print(f"- {m.name} (Supports generateContent)")
+        print(f"- {m.name} (Supports generateContent)")"""
 
 # Initialize the model
 model = genai.GenerativeModel('gemini-1.5-flash')
